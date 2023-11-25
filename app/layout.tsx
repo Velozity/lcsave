e113298b6577c797fe58074045e9ff32e03db3c8
Velozity <zacaryzippy@gmail.com>
2023-11-25 18:51:28 +1100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        async
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-B9C8LVZLWC"
+      ></Script>
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-B9C8LVZLWC', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+
       <body className={inter.className}>{children}</body>
     </html>
   );
