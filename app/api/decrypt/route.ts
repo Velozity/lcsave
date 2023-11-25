14c7f40d1273f43d0ad7f284912679bb206f8244
Velozity <zacaryzippy@gmail.com>
2023-11-25 15:32:04 +1100

    init

---
+++ app/api/decrypt/route.ts
@@ -0,0 +1,24 @@
+import { decrypt } from "@/lib/crypto";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const data = await request.formData();
+  const file = data.get("file");
+  if (!file || !(file instanceof Blob)) {
+    return NextResponse.json({
+      success: false,
+      message: "No file provided or file is not a Blob.",
+    });
+  }
+
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+
+  try {
+    const decrypted = decrypt(buffer);
+    return NextResponse.json({ success: true, decrypted });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ success: false, message: "Decryption failed." });
+  }
+}
